1be3dedaaddf18f86e6e99bb1c8bfb91b9b3192d
Yousaf <yousafishaq13@gmail.com>
2022-09-09 10:04:06 +0200

    updated imports

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 import theme from './theme'
 import { ThemeProvider } from '@emotion/react'
 
@@ -14,5 +13,3 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>,
 )
-
-reportWebVitals()
